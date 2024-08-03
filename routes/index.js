051fbf5922a020d920ed29d55d00b14f84d5ec1d
wiseman-umanah <wisemanumanah@gmail.com>
2024-08-03 16:45:51 +0100

    Added a new endpoint

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const route = express.Router();
 const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
 
 route.get('/status', AppController.getStatus);
 route.get('/stats', AppController.getStats);
+route.get('/users', UsersController.postNew);
 
 module.exports = route;
