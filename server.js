0dacf9b656e3cd66c659565984cdbd0ae97c3fa2
wiseman-umanah <wisemanumanah@gmail.com>
2024-08-07 19:49:20 +0100

    Started and completed

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const express = require('express');
-const process = require('process');
-const route = require('./routes/index');
+import express from 'express';
+import controllerRouting from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use('/', route);
+app.use(express.json());
 
-app.listen(port);
+controllerRouting(app);
 
-module.exports = app;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+export default app;
--- utils/db.js
+++ utils/db.js
@@ -5,6 +5,9 @@ const DB_PORT = process.env.DB_PORT || 27017;
 const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
 const url = `mongodb://${DB_HOST}:${DB_PORT}`;
 
+/**
+ * Class for performing operations with Mongo service
+ */
 class DBClient {
   constructor() {
     MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
@@ -20,15 +23,27 @@ class DBClient {
     });
   }
 
+  /**
+   * Checks if connection to Redis is Alive
+   * @return {boolean} true if connection alive or false if not
+   */
   isAlive() {
     return Boolean(this.db);
   }
 
+  /**
+   * Returns the number of documents in the collection users
+   * @return {number} amount of users
+   */
   async nbUsers() {
     const numberOfUsers = this.usersCollection.countDocuments();
     return numberOfUsers;
   }
 
+  /**
+   * Returns the number of documents in the collection files
+   * @return {number} amount of files
+   */
   async nbFiles() {
     const numberOfFiles = this.filesCollection.countDocuments();
     return numberOfFiles;
